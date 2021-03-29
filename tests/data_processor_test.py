a0a8571d007bab531f33c51b24724278e429cbab
Steven E. Waldren <swaldren@gmail.com>
2021-03-29 18:22:46 -0500

    working on models

--- tests/data_processor_test.py
+++ tests/data_processor_test.py
@@ -12,7 +12,7 @@ import tensorflow as tf
 
 aapl = yf.Ticker('AAPL')
 
-df = aapl.history(start='2017-01-01', end='2020-01-01')
+df = aapl.history(start='2000-01-01', end='2020-01-01')
 
 pp = data_processor.PreProcessor(df, date='Date', open='Open', high='High', low='Low', close='Close', volume='Volume')
 
@@ -27,7 +27,7 @@ print(df.columns)
 print("***************************************")
 
 train, validate, test = np.split(df, [int(.6*len(df)), int(.8*len(df))])
-input_width = 10
+input_width = 5
 label_width = 1
 shift = 1
 y = ['Close_pct_change_log']
@@ -37,20 +37,31 @@ for example_inputs, example_labels in wp.train.take(1):
   print(f'Inputs shape (batch, time, features): {example_inputs.shape}')
   print(f'Labels shape (batch, time, features): {example_labels.shape}')
 
+bl = Baseline()
+
 dense = tf.keras.Sequential([
     tf.keras.layers.Dense(units=64, activation='relu'),
     tf.keras.layers.Dense(units=64, activation='relu'),
     tf.keras.layers.Dense(units=1)
 ])
 
+conv_model = tf.keras.Sequential([
+    tf.keras.layers.Conv1D(filters=32,
+                           kernel_size=(3,),
+                           activation='relu'),
+    tf.keras.layers.Dense(units=32, activation='relu'),
+    tf.keras.layers.Dense(units=1),
+])
+
 mr = ModelRunner()
 val_performance = {}
 performance = {}
-bl = Baseline()
-history = mr.compile_and_fit(bl, wp)
 
-val_performance['Model'] = bl.evaluate(wp.val)
-performance['Model'] = bl.evaluate(wp.test, verbose=0)
+model = conv_model
+history = mr.compile_and_fit(model, wp)
+
+val_performance['Model'] = model.evaluate(wp.val)
+performance['Model'] = model.evaluate(wp.test, verbose=0)
 
 for name, value in performance.items():
   print(f'{name:15s}: {value[1]:0.4f}')
